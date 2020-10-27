6173390255a9a0afd704b41c60602bb7ccfa9c37
varunpbardwaj <varunbardwajp@gmail.com>
2020-10-27 11:08:50 +0530

    Update Badge

---
+++ lib/components/badges/index.d.ts
@@ -0,0 +1,4 @@
+/// <reference types="react" />
+import BadgeProps from "../../props/badge.props";
+declare function Badge(props: BadgeProps): JSX.Element;
+export default Badge;
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,4 +1,5 @@
 export { default as Banner } from "./components/banners/banner";
 export { default as CustomBanner } from "./components/banners/custom-banner";
+export { default as Badge } from "./components/badges";
 export { default as Toast } from "./components/toasts/toast";
 export { default as CustomToast } from "./components/toasts/custom-toast";
